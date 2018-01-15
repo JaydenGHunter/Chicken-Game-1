77a7e87b376f82bb4fd7f85cf39e59d689af86df
Jayden - laptop <jaydenghunter96@gmail.com>
2018-01-15 20:01:40 +0800

    Added new objects

---
+++ Chicken Game/Assets/Scripts/Chicken.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Chicken", menuName = "Objects/Chicken", order = 1)]
+public class Chicken : ScriptableObject {
+    public new string name;
+    public int speed = 20;
+}
